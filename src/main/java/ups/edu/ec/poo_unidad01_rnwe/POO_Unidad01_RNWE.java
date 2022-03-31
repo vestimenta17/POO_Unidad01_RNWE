4d6878a97bafdbd9959a59ffd1e252ccbb79d70a
PC-01 <PC-01@DESKTOP-FIPVH18>
2022-03-31 14:16:26 +0200

    Otra vez lo cambio xq ando aprendiendo xD aqui va metodos y clase weno que lo terminamos

---
+++ src/main/java/ups/edu/ec/poo_unidad01_rnwe/POO_Unidad01_RNWE.java
@@ -0,0 +1,97 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package ups.edu.ec.poo_unidad01_rnwe;
+
+/**
+ *
+ * @author PC-01
+ */
+public class POO_Unidad01_RNWE {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
